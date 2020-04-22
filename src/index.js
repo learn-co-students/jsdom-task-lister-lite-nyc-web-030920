56268b0e13dac3641005221d67f627a28093c7a9
Anil Gurung <anil_grg@hotmail.com>
2020-04-22 15:11:54 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  
+  // function submitValue(){
+  
+  // console.log(input)
+  
+  const submit = document.getElementById('create-task-form')
+  
+  submit.addEventListener('submit',function(event){
+  //  console.log(submitBtn);
+  event.preventDefault();
+    
+    const input = document.getElementById('new-task-description')
+    
+    const ul = document.getElementById('tasks');
+    
+    const li = document.createElement('li')
+    
+    li.innerText = input.value;
+    
+    // console.log(input.value)
+    
+    ul.append(li);
+
+    submit.reset();
+    
+  })
+  
+
+  // console.log(submit.children[1].value)
+
+})
