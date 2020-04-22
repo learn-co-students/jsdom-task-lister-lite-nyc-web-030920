afcfa2e29978ef829a86371ffea3590b75ea87e1
github username <github email address>
2020-04-22 16:42:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const form = document.getElementById('create-task-form')
+    form.addEventListener('submit', function(event) {
+      event.preventDefault()
+      const taskInput = document.getElementById("new-task-description").value
+      const list = document.getElementById('tasks')
+      let listItem = document.createElement('li')
+      listItem.innerText = taskInput
+      list.append(listItem.innerText)
+    });
+  });
+
